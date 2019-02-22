a76fccb63e66f5e577265bcb26eb6ca4c59eecd5
Garcia Rosario,Yori Y.P <y.garciarosario@student.fontys.nl>
2019-02-22 09:18:22 +0100

    added login screen firebase

---
+++ app/src/test/java/com/example/socialmusic/FireBaseTests.java
@@ -0,0 +1,11 @@
+package com.example.socialmusic;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.junit.Test;
+
+public class FireBaseTests {
+    @Test
+    void getUserValueFromFireBase()
+    {
+    }
+}
